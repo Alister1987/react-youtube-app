5da93db1b0086917591d3c8897425b192dfcc55d
Alex Strelnik <alisterr1987@gmail.com>
2020-01-31 15:10:41 +0200

    Refactored code using custom hook "useInputValue"

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -7,20 +7,34 @@ const styles = {
     }
 };
 
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = useState(defaultValue);
+
+    return {
+        bind: {
+            value: value,
+            onChange: event => setValue(event.target.value)
+        },
+        value: value,
+        clear: () => setValue('')
+    }
+}
+
 function AddTodo({onCreate}) {
+
+    const input = useInputValue('');
 
     function submitHandler(event) {
         event.preventDefault();
 
-        if (value.trim()) {
-            onCreate(value);
-            setValue('');
+        if (input.value.trim()) {
+            onCreate(input.value);
+            input.clear();
         }
     }
-    const [value, setValue] = useState('');
     return (
         <form style={styles.form} onSubmit={submitHandler}>
-            <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+            <input type="text" {...input.bind}/>
             <button type="submit">Add todo</button>
         </form>
     )
